1e0299c9e7729186c29986f38ccdb5a31ec99029
Henr. Ferreira Saraiva <rickferr.dev@gmail.com>
2026-07-30 13:15:54 -0300

    feat(templates): add license, privacy, and terms templates for legal compliance
    - Create LICENSE_CONTENT, PRIVACY_CONTENT, and TERMS_CONTENT for user agreements
    - Structure templates in a new index file for easy access and management

---
+++ src/templates/index.tsx
@@ -0,0 +1,19 @@
+import LICENSE_CONTENT from "./license";
+import PRIVACY_CONTENT from "./privacy";
+import TERMS_CONTENT from "./terms";
+
+const TEMPLATES = {
+	TERMS: {
+		CONTENT: TERMS_CONTENT,
+	},
+
+	PRIVACY: {
+		CONTENT: PRIVACY_CONTENT,
+	},
+
+	LICENSE: {
+		CONTENT: LICENSE_CONTENT,
+	},
+};
+
+export default TEMPLATES;
